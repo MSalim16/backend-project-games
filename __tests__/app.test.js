837dbf3c8ddf32f713fd2837291ed4a39864c81c
MSalim16 <msalim16@DESKTOP-DAK50HL.localdomain>
2022-12-15 13:48:36 +0000

    ticket 9 done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -337,3 +337,28 @@ describe("PATCH /api/reviews/review_id", () => {
       });
   });
 });
+describe("/api/users", () => {
+  test("GET - STATUS: 200, responds with an array of the newly inserted user objects", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .then(({ body: { user } }) => {
+        expect(users).toHaveLength(4);
+        users.forEach((user) => {
+          expect(user).toMatchObject({
+            username: expect.any(String),
+            name: expect.any(String),
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
+  test("404: responds with correct error status when invalid path has been used", () => {
+    return request(app)
+      .get("/api/userusersss")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Invalid Path");
+      });
+  });
+});
