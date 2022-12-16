f05dcb41b44279a4a54a71cbde5786ad73dca374
MSalim16 <msalim16@DESKTOP-DAK50HL.localdomain>
2022-12-16 14:50:06 +0000

    ticket 11 done

--- models/reviews.js
+++ models/reviews.js
@@ -68,7 +68,10 @@ exports.selectReviews = (category, sort_by = "created_at", order = "DESC") => {
 
 exports.fetchReviewByID = (id) => {
   return db
-    .query("SELECT * FROM reviews WHERE review_id = $1", [id])
+    .query(
+      "SELECT reviews.*, COUNT (comment_id) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id WHERE reviews.review_id = $1 GROUP BY reviews.review_id;",
+      [id]
+    )
     .then(({ rows }) => {
       if (rows.length === 0)
         return Promise.reject({
